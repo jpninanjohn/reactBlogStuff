77fa88f35c399924bb99556f3df384f0f148c297
Ninan <jninan@thoughtwork.com>
2017-02-05 09:41:54 +0530

    #1 [Ninan/ReactDocs] Using keys and lists

---
+++ src/Components/Blog.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Content from './Content'
+
+function Blog(props) {
+    const titleList = props.posts
+        .map(
+            (post) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+        );
+
+    const contentList = props.posts
+        .map(
+            (post) =>
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <div>{post.content}</div>
+                </div>
+        );
+
+    return (
+        <div>
+            <Sidebar listOfPosts={titleList}/>
+            <Content content={contentList}/>
+        </div>
+    );
+}
+
+export default Blog;
